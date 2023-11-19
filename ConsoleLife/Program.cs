d65cfdd5d332088adefdfe41007346132ebe6af4
billy <billy@BABY>
2023-11-19 10:38:13 +0300

    Добавьте файлы проекта.

---
+++ ConsoleLife/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleLife
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+            Console.CursorVisible = false;
+            Console.SetCursorPosition(0, 0);
+
+            var gameEngine = new GameEngine
+            (
+                rows: 360,
+                cols: 3000,
+                density: 2
+            );
+
+            while (true)
+            {
+                Console.Title = gameEngine.CurrentGeneration.ToString();
+
+                var field = gameEngine.GetCurrentGeneration();
+
+                for (int y = 0; y < field.GetLength(1); y++ )
+                {
+                    var str = new char[field.GetLength(0)];
+                    for(int x = 0; x < field.GetLength(1); x++)
+                    {
+                        if (field[x, y])
+                            str[x] = '#';
+                        else
+                            str[x] = ' ';
+                    }
+                    Console.WriteLine(str);
+                }
+                Console.SetCursorPosition(0, 0);
+                gameEngine.NextGeneration();
+            }
+        }
+    }
+}
---
+++ GameOfLife/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("GameOfLife")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GameOfLife")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("8f28db6b-19e4-4b28-94d6-60ba3c541ae4")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
